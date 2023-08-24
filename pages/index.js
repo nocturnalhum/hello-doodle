a3ddf7ac7a1624658ad87e472077eb7159557097
nocturnal_hum <thanin.chan@gmail.com>
2023-08-24 16:43:26 -0400

    Cleaned up unnecessary imports and functions from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,12 @@
+import { useCanvasContext } from '@/contextAPI/context';
 import Card1 from '@/components/Card1';
 import Card2 from '@/components/Card2';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
-import { useCanvasContext } from '@/contextAPI/context';
-import Image from 'next/image';
 
 export default function Home() {
-  const {
-    canvasRef,
-    isFlipped,
-    isModalOpen,
-    setIsModalOpen,
-    setElements,
-    showTools,
-  } = useCanvasContext();
+  const { isFlipped, showTools } = useCanvasContext();
 
-  const handleConfirmClear = () => {
-    let canvas = canvasRef.current;
-    let context = canvas.getContext('2d');
-    context.fillStyle = '#FFF'; // Set the background color to white
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    setElements([]);
-    setIsModalOpen(false);
-  };
-
-  const handleCancelClear = () => {
-    setIsModalOpen(false);
-  };
   return (
     <main
       className={`flex h-screen min-h-screen items-center justify-between bg-gradient-to-br from-slate-700 via-blue-300 to-gray-400`}
@@ -49,9 +29,7 @@ export default function Home() {
           className={`absolute top-10 duration-300 ${
             !showTools ? '-translate-x-[100%]' : 'translate-x-[0%]'
           } `}
-        >
-          {/* <Tools /> */}
-        </div>
+        ></div>
         <div
           className={`absolute top-0 duration-300 ${
             showTools ? 'translate-y-[100%]' : 'translate-y-0'
