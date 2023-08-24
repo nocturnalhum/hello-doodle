4769eba84d1b5fcf77edf627bb1e9af8889bd05d
nocturnal_hum <thanin.chan@gmail.com>
2023-08-24 14:41:09 -0400

    Initial set-up and test mobile screen orientation

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className='bg-green-500 portrait:h-[5rem] portrait:w-full landscape:h-full landscape:w-[5rem]'>
+      Footer
+    </div>
+  );
+}
---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className='portrait:h-[5rem] portrait:w-full landscape:h-full landscape:w-[5rem] bg-green-500'>
+      Header
+    </div>
+  );
+}
