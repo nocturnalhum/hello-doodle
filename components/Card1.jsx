53cf8f08af1e6ec88c1f4ef68a82ced961c831b3
nocturnal_hum <thanin.chan@gmail.com>
2023-09-04 08:23:57 -0400

    Changed height to dvh. Prevent canvas from being selected during drawing.

--- components/Card1.jsx
+++ components/Card1.jsx
@@ -5,7 +5,7 @@ export default function Card1() {
   const elementRef = useRef();
 
   return (
-    <div ref={elementRef} className='h-full w-full touch-none'>
+    <div ref={elementRef} className='h-full w-full touch-none select-none'>
       <Canvas elementRef={elementRef} />
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
 
   return (
     <main
-      className={`flex h-screen min-h-screen items-center justify-between bg-gradient-to-br from-slate-700 via-blue-300 to-gray-400`}
+      className={`flex h-[100dvh] min-h-[100dvh] items-center justify-between bg-gradient-to-br from-slate-700 via-blue-300 to-gray-400`}
     >
       <ConfirmationModal
         isOpen={isModalOpen}
